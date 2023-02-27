0c95205b3e0e76fa341d1e3eccb41beffc209f72
Alex Sklyarov <alexSklayr97@gmail.com>
2023-02-27 04:30:41 +0200

    end with backend

--- src/context/FeedbackContext.js
+++ src/context/FeedbackContext.js
@@ -1,4 +1,3 @@
-import {v4 as uuidv4} from 'uuid'
 import { createContext, useState, useEffect } from "react";
 
 const FeedbackContext = createContext()
@@ -23,20 +22,42 @@ const fetchFeedback = async () => {
     setIsLoading(false)
 }
 
-const updateFeedback = (id, updItem) => {
-    setFeedback(() => feedback.map((item)=>item.id === id ? {...item, ...updItem} : item))
+const addFeedback = async (newFeedback) => {
+    const response = await fetch(`http://localhost:5000/feedback`, {
+        method: "POST",
+        headers: {
+        "Content-Type": "application/json"
+        },
+        body: JSON.stringify(newFeedback),
+    });
+    const data = await response.json()
+    setFeedback([data, ...feedback])
+    };
+
+
+
+const updateFeedback = async (id, updItem) => {
+    const response = await fetch(`http://localhost:5000/feedback/${id}`, {
+        method: "PUT",
+        headers: {
+        "Content-Type": "application/json"
+        },
+        body: JSON.stringify(updItem),
+    });
+    const data = await response.json()
+    setFeedback(() => feedback.map((item)=>item.id === id ? {...item, ...data} : item))
 }
 
-const deleteFeedback = (id) =>{
+const deleteFeedback = async (id) =>{
     if(window.confirm('Are u sure u want to delete?')){
+        await fetch(`http://localhost:5000/feedback/${id}`, {
+            method: "DELETE",
+        });
     setFeedback(() => feedback.filter(item=>(item.id!==id)))
     }
 }
 
-const addFeedback = (newFeedback) =>{
-    newFeedback.id = uuidv4()
-    setFeedback(() => [newFeedback,...feedback])
-    }
+
 
 const editFeedback = (item) =>{
 setFeedbackEdit(() => {
